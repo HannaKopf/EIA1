1cefcd107c9676742ba17fd25af8af25f73eacf1
HannaKopf <92110464+HannaKopf@users.noreply.github.com>
2021-11-27 16:11:10 +0100

    L07 vorläufig

---
+++ L07/script.js
@@ -0,0 +1,35 @@
+window.addEventListener("load", function () {
+    /* Sounds einfügen*/
+    var sound;
+    var kick = new Audio('assets/kick.mp3');
+    var hihat = new Audio('assets/hihat.mp3');
+    var snare = new Audio('assets/snare.mp3');
+    var A = new Audio('assets/A.mp3');
+    var C = new Audio('assets/C.mp3');
+    var F = new Audio('assets/F.mp3');
+    var G = new Audio('assets/G.mp3');
+    var laugh1 = new Audio('assets/laugh-1.mp3');
+    var laugh2 = new Audio('assets/laugh-2.mp3');
+    var beat = [kick, hihat, snare, hihat];
+    /* Funktion Sounds abspielen*/
+    function playSample(sound) {
+        sound.play();
+    }
+    /* Funktion Beat abspielen*/
+    function playBeat(beat) {
+        setInterval(function () { beat.play(); }, 3000);
+    }
+    //setInterval(function() {    // Anweisungen }, 500);
+    /* Event Button klick*/
+    document.querySelector("#pad1").addEventListener("click", function () { playSample(kick); });
+    document.querySelector("#pad2").addEventListener("click", function () { playSample(hihat); });
+    document.querySelector("#pad3").addEventListener("click", function () { playSample(snare); });
+    document.querySelector("#pad4").addEventListener("click", function () { playSample(A); });
+    document.querySelector("#pad5").addEventListener("click", function () { playSample(C); });
+    document.querySelector("#pad6").addEventListener("click", function () { playSample(F); });
+    document.querySelector("#pad7").addEventListener("click", function () { playSample(G); });
+    document.querySelector("#pad8").addEventListener("click", function () { playSample(laugh1); });
+    document.querySelector("#pad9").addEventListener("click", function () { playSample(laugh2); });
+    document.querySelector("#play").addEventListener("click", function () { playBeat; });
+});
+//# sourceMappingURL=script.js.map
---
+++ L07/script.ts
@@ -0,0 +1,38 @@
+window.addEventListener("load",function() {
+    /* Sounds einfügen*/
+        var sound:HTMLAudioElement;
+
+        var kick:HTMLAudioElement = new Audio('assets/kick.mp3');
+        var hihat:HTMLAudioElement = new Audio('assets/hihat.mp3');
+        var snare:HTMLAudioElement = new Audio('assets/snare.mp3');
+        var A:HTMLAudioElement = new Audio('assets/A.mp3');
+        var C:HTMLAudioElement = new Audio('assets/C.mp3');
+        var F:HTMLAudioElement = new Audio('assets/F.mp3');
+        var G:HTMLAudioElement = new Audio('assets/G.mp3');
+        var laugh1:HTMLAudioElement = new Audio('assets/laugh-1.mp3');
+        var laugh2:HTMLAudioElement = new Audio('assets/laugh-2.mp3');
+
+        var beat:HTMLAudioElement []=[kick, hihat, snare, hihat];
+
+    /* Funktion Sounds abspielen*/
+        function playSample (sound): void{
+            sound.play();
+        }
+
+    /* Funktion Beat abspielen*/
+        function playBeat (beat): void {
+           setInterval(function(){ beat.play(); }, 3000);
+        }
+        //setInterval(function() {    // Anweisungen }, 500);
+    /* Event Button klick*/
+        document.querySelector("#pad1").addEventListener("click", function() {playSample(kick);});
+        document.querySelector("#pad2").addEventListener("click", function() {playSample(hihat);});
+        document.querySelector("#pad3").addEventListener("click", function() {playSample(snare);});
+        document.querySelector("#pad4").addEventListener("click", function() {playSample(A);});
+        document.querySelector("#pad5").addEventListener("click", function() {playSample(C);});
+        document.querySelector("#pad6").addEventListener("click", function() {playSample(F);});
+        document.querySelector("#pad7").addEventListener("click", function() {playSample(G);});
+        document.querySelector("#pad8").addEventListener("click", function() {playSample(laugh1);});
+        document.querySelector("#pad9").addEventListener("click", function() {playSample(laugh2);});
+        document.querySelector("#play").addEventListener("click", function() {playBeat;});
+});
